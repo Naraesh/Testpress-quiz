be6c974de2b50bb85456f7c810d411a1cc40ecea
Naraesh <naraeshkumar96@gmail.com>
2020-08-23 20:29:24 +0530

    final

--- QuizProject/settings.py
+++ QuizProject/settings.py
@@ -72,26 +72,24 @@ TEMPLATES = [
 WSGI_APPLICATION = 'QuizProject.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.mysql',
-       'NAME': 'testpress',
-       'USER': 'root',
-       'PASSWORD': '',
-       'HOST': 'localhost',
-       'PORT': '',
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': '',
+#        'USER': 'root',
+#        'PASSWORD': '',
+#        'HOST': 'localhost',
+#        'PORT': '',
+#    }
+# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
